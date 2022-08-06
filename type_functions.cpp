42d8b762eb1a3c9d5858d7122d12c16ff0777fea
ishansheth <ishansheth@gmail.com>
2022-08-06 11:35:10 +0200

    better type function example

--- type_functions.cpp
+++ type_functions.cpp
@@ -24,17 +24,19 @@ struct B
 
 constexpr int on_stack_max = sizeof(int);
 
-template<typename T>
+template<typename T,std::size_t N>
 struct On_heap
 {
-  On_heap(): p(new T[100]){std::cout<<"allocating on heap"<<std::endl;}
+  On_heap(): p(new T[N]){std::cout<<"allocating on heap"<<std::endl;}
   ~On_heap(){delete p;}
 
-  T& operator*()
+  // input as an index, return the reference of the element at that index
+  T& operator*(std::size_t index)
   {
-    return *p;
+    return *(p+index);
   }
 
+  // input as an index, return the element at that index
   T* operator->()
   {
     return p;
@@ -42,7 +44,7 @@ struct On_heap
 
   T& operator[](std::size_t index)
   {
-    return p[index];
+    return *(p+index);
   }
 
   On_heap(const On_heap&) = delete;
@@ -52,38 +54,27 @@ private:
   T* p;
 };
 
-template<typename T>
+template<typename T,std::size_t N>
 struct Scoped
 {
   Scoped(){std::cout<<"allocating on stack"<<std::endl;}
-
-  T& operator*()
-  {
-    return p;
-  }
-
-  T* operator->()
-  {
-    return &p;
-  }
 
   T& operator[](std::size_t index)
   {
     return p[index];
   }
 
-
   Scoped(const Scoped&) = delete;
   Scoped& operator=(const Scoped&) = delete;
 
 private:
-  T p[100];
+  T p[N];
 };
 
-template<typename T>
+template<typename T,std::size_t N>
 struct Obj_holder
 {
-  using type = typename std::conditional<(sizeof(T)<=on_stack_max),Scoped<T>,On_heap<T>>::type;
+  using type = typename std::conditional<(sizeof(T)<=on_stack_max),Scoped<T,N>,On_heap<T,N>>::type;
 };
 
 
@@ -107,11 +98,20 @@ struct Y
 
 void f()
 {
-  typename Obj_holder<char>::type v1;    // v1 type is Scoped<double>, on stack
-  typename Obj_holder<double>::type v2;   //v2 type is On_heap<std::array>, on heap
+  typename Obj_holder<char,20>::type v1;    // v1 type is Scoped<double>, on stack
+  typename Obj_holder<double,20>::type v2;   //v2 type is On_heap<std::array>, on heap
 
-  v1[10] = 7.7;
-  v2[77] = 9.9;
+  for(unsigned int i = 0; i < 10; i++)
+  {
+     v1[i] = i;
+     v2[i] = i+1;   
+  }
+
+  for(unsigned int i = 0; i < 10; i++)
+  {
+    std::cout<<"stack:"<<int(v1[i])<<std::endl; // since its an array of char, count can't print without casting
+    std::cout<<"heap:"<<v2[i]<<std::endl;
+  }
 
 }
 
